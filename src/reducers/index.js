eceff07ced5c7310fa0eeb05e7f6958824984d9d
Vitaliy <puga.vitaliy@gmail.com>
2018-05-20 09:28:16 +0300

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import initialReducer from './initial_reducer';
+import {combineReducers} from 'redux';
+
+const rootReducer = combineReducers({
+    initialReducer
+})
+
+export default rootReducer;
